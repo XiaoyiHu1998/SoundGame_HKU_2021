eb8d40f02e42bd7faa1ad93716f87007f0df0a0c
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2021-10-09 17:29:51 +0200

    added scripts, player and cage are still placeholders but creature should have working movement and spellEffects, not tested yet though

---
+++ SoundGame_hearder/Assets/scripts/Creature.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public Player player;
+    public Cage cage;
+    public float minMoveTime;
+    public float maxMoveTime;
+    public float minSoundTime;
+    public float maxSoundTime;
+    public float minCageDistance;
+    public float maxCageDistance;
+    public float runFromPlayerDistance;
+
+    public float clapDistance;
+    public float clapTime;
+    public float lureDistance;
+    public float lureTime;
+
+    public float moveSpeed;
+    public float runSpeed;
+
+    public float divineEffectCooldown;
+
+    private bool doneMoving;
+    private bool nearPlayer;
+    private bool cageTooClose;
+    private bool cageTooFar;
+
+
+    private Stopwatch moveTimer;
+    private System.Random random;
+    private Vector3 moveDirection;
+    private float moveTime;
+
+    private bool divineEffect;
+    private float divineEffectTime;
+    private Stopwatch divineEffectTimer;
+    private Vector3 divineEffectDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveTimer = new Stopwatch();
+        moveTimer.Start();
+        divineEffectTimer = new Stopwatch();
+        random = new System.Random();
+        doneMoving = true;
+        nearPlayer = false;
+        cageTooClose = false;
+        cageTooFar = false;
+        divineEffect = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkState();
+        setMoveDirection();
+        Move();
+    }
+
+    private void checkState()
+    {
+        nearPlayer = Vector3.Distance(player.GetLocation(), gameObject.transform.position) <= runFromPlayerDistance;
+        cageTooFar = Vector3.Distance(cage.GetLocation(), gameObject.transform.position) >= maxCageDistance;
+        cageTooClose = Vector3.Distance(cage.GetLocation(), gameObject.transform.position) <= minCageDistance;
+        doneMoving = moveTimer.ElapsedMilliseconds / 1000 >= moveTime;
+
+        if (divineEffect)
+        {
+            divineEffect = divineEffectTime <= divineEffectTimer.ElapsedMilliseconds / 1000;
+            if (!divineEffect)
+            {
+                divineEffectTimer.Restart();
+                moveTimer.Start();
+            }
+        }
+    }
+
+    private void setMoveDirection()
+    {
+        Vector3 playerToCreature = (player.GetLocation() - gameObject.transform.position).normalized;
+        Vector3 cageToCreature = (cage.GetLocation() - gameObject.transform.position).normalized;
+        if (divineEffect)
+        {
+            return;
+        }
+        else if (nearPlayer)
+        {
+            moveDirection = playerToCreature;
+        }
+        else if (cageTooClose)
+        {
+            moveDirection += cageToCreature;
+            moveDirection.Normalize();
+        }
+        else if (cageTooFar)
+        {
+            moveDirection -= cageToCreature;
+            moveDirection.Normalize();
+        }
+        else if (doneMoving)
+        {
+            doneMoving = false;
+            bool negativeX = random.Next(0, 2) <= 1;
+            bool negativeZ = random.Next(0, 2) <= 1;
+
+            float moveX = (float)random.NextDouble();
+            float moveZ = (float)random.NextDouble();
+
+            if (negativeX)
+                moveX *= -1f;
+            if (negativeZ)
+                moveZ *= -1f;
+
+            moveDirection = new Vector3(moveX, 0, moveZ).normalized;
+
+            moveTime = (float)(minMoveTime + random.NextDouble() * (maxMoveTime - minMoveTime));
+            moveTimer.Restart();
+        }
+    }
+
+    private void Move()
+    {
+        if (divineEffect)
+        {
+            gameObject.transform.position += divineEffectDirection * runSpeed;
+        }
+        else
+        {
+            float movementSpeed;
+            if (nearPlayer)
+            {
+                movementSpeed = runSpeed;
+            }
+            else
+            {
+                movementSpeed = moveSpeed;
+            }
+            gameObject.transform.position += moveDirection * movementSpeed;
+        }
+    }
+
+    public void Clap()
+    {
+        if(!divineEffect && divineEffectTimer.ElapsedMilliseconds / 1000 >= divineEffectCooldown)
+        {
+            divineEffect = true;
+            moveTimer.Stop();
+            divineEffectTimer.Restart();
+            divineEffectTime = clapTime;
+            divineEffectDirection = (player.GetLocation() - gameObject.transform.position).normalized;
+        }
+    }
+
+    public void Lure()
+    {
+        if(!divineEffect && divineEffectTimer.ElapsedMilliseconds / 1000 >= divineEffectCooldown)
+        {
+            divineEffect = true;
+            moveTimer.Stop();
+            divineEffectTimer.Restart();
+            divineEffectTime = lureTime;
+            divineEffectDirection = (gameObject.transform.position - player.GetLocation()).normalized;
+        }
+    }
+}
