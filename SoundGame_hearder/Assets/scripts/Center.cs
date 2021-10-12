485ec64b99e01b9ae3264db5cb1e57a668140091
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2021-10-12 11:55:49 +0200

    renamed Cage to Center for clarity

--- SoundGame_hearder/Assets/scripts/Center.cs
+++ SoundGame_hearder/Assets/scripts/Center.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cage : MonoBehaviour
+public class Center : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
