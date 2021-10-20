77aaa1971ce79c54b840fe745b5cf59195269107
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2021-10-20 10:59:54 +0200

    made godSource support playing only a list of allowed audioClips and being able to set or unset that mode

--- SoundGame_hearder/Assets/scripts/GodSource.cs
+++ SoundGame_hearder/Assets/scripts/GodSource.cs
@@ -6,9 +6,12 @@ public class GodSource : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource audioSource;
+    private List<AudioClip> allowedClips;
+    private bool onlyAllowedClips;
     void Start()
     {
-        
+        onlyAllowedClips = false;
+        allowedClips = new List<AudioClip>();
     }
 
     // Update is called once per frame
@@ -19,9 +22,20 @@ public class GodSource : MonoBehaviour
 
     public void PlayLine(AudioClip audioClip, bool overrideCurrentLine)
     {
-        if (audioSource.isPlaying && !overrideCurrentLine)
+        if (audioSource.isPlaying && !overrideCurrentLine || onlyAllowedClips && !allowedClips.Contains(audioClip))
             return;
         audioSource.clip = audioClip;
         audioSource.Play();
+    }
+
+    public void setAllowedList(List<AudioClip> allowedClipsList)
+    {
+        onlyAllowedClips = true;
+        allowedClips = allowedClipsList;
+    }
+
+    public void allowAll()
+    {
+        onlyAllowedClips = false;
     }
 }
